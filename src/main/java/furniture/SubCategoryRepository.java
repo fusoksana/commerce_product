2703d7f6eb20982fecd69f1846b5bbe90796a4d9
oksana <fusoksana@gmail.com>
2018-04-14 19:26:27 +0300

    removed unnecessary code

--- src/main/java/furniture/SubCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package furniture;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubCategoryRepository extends JpaRepository<SubCategory,Integer> {
-    SubCategory findBySubCategoryId( int subCategoryId);
-}
