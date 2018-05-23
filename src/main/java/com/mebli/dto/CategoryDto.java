f296d50621173202c693433c2859bbfae06dc777
oksana <fusoksana@gmail.com>
2018-05-23 16:15:49 +0300

    Made categoryDTO

---
+++ src/main/java/com/mebli/dto/CategoryDto.java
@@ -0,0 +1,41 @@
+package com.mebli.dto;
+
+import com.mebli.domain.SubCategory;
+
+import java.util.List;
+
+public class CategoryDto {
+
+    private int categoryID;
+
+    private String categoryName;
+
+    private List<SubCategory> subCategories;
+
+    public List<SubCategory> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(List<SubCategory> subCategories) {
+        this.subCategories = subCategories;
+    }
+
+    public CategoryDto() {
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
